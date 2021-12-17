7976bbd83806240f14098277b0535dd22392c3aa
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 02:36:45 +0300

    Scripted auto-commit on change (2021-12-18 02:36:45) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -12,9 +12,7 @@ public class StorageActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(TestResult.class, r -> {
-
-                })
+                .match()
                 .build();
     }
 }
