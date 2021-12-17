361dda9b68bb4081e7e2cefb6d78bdfc197758a4
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 02:00:14 +0300

    Scripted auto-commit on change (2021-12-18 02:00:14) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -3,4 +3,8 @@ package lab4.actors;
 import akka.actor.AbstractActor;
 
 public class StorageActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
