4224a5b165ca9967eb92a3e571d0f4b8545b2e0f
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 00:56:49 +0300

    Scripted auto-commit on change (2021-12-18 00:56:49) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -2,11 +2,12 @@ package lab4;
 
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class TaskTestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("webtest");
         final Http http = Http.get(system);
-        final A
+        final ActorMaterializer materializer
     }
 }
