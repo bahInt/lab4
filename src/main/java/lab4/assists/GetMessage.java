1cf81cc38633fade97d050fdebb0fba093e101df
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 02:59:49 +0300

    Scripted auto-commit on change (2021-12-18 02:59:49) by gitwatch.sh

--- src/main/java/lab4/assists/GetMessage.java
+++ src/main/java/lab4/assists/GetMessage.java
@@ -1,6 +1,11 @@
 package lab4.assists;
 
 public class GetMessage {
+    private final String packID;
+
+    public GetMessage(String packID, String result) {
+        this.packID 
+    }
     public String getPackID() {
     }
 }
