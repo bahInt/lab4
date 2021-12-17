bcb35481a0d15ff00e7f3ad566651ecce57791aa
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 00:49:55 +0300

    Scripted auto-commit on change (2021-12-18 00:49:55) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -5,6 +5,6 @@ import akka.actor.ActorSystem;
 public class TaskTestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("webtest");
-        final Http http = http
+        final Http http = 
     }
 }
