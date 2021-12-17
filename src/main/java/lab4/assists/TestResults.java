2de594b3104a590202baa313ca48b408243e1600
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 02:29:23 +0300

    Scripted auto-commit on change (2021-12-18 02:29:22) by gitwatch.sh

--- src/main/java/lab4/assists/TestResults.java
+++ src/main/java/lab4/assists/TestResults.java
@@ -3,5 +3,5 @@ package lab4.assists;
 import java.io.Serializable;
 
 public class TestResults implements Serializable {
-    private static final long
+    private static final long serialVersionUID = 1L;
 }
