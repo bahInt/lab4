151e2acdff5317ed93f513fe5fa900ce67d34acf
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 01:44:11 +0300

    Scripted auto-commit on change (2021-12-18 01:44:11) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -12,7 +12,7 @@ import java.util.concurrent.CompletionStage;
 
 public class TaskTestingApp {
     private static final int PORT = 8088;
-    private 
+    private static final String HOST = "localhost";
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("webtest");
         final Http http = Http.get(system);
