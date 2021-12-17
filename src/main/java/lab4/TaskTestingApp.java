ab2f0d235a4238d00a8c94d5d3d9435f9fe1d299
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 01:32:20 +0300

    Scripted auto-commit on change (2021-12-18 01:32:20) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -3,10 +3,12 @@ package lab4;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import java.util.concurrent.CompletionStage;
 
 public class TaskTestingApp {
     public static void main(String[] args) {
@@ -14,5 +16,6 @@ public class TaskTestingApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         //final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+        //final CompletionStage<ServerBinding> binding = http.bindAndHandle()
     }
 }
