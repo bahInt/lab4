caf1516951b0956caf5efd23e572153486f04c39
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 01:45:19 +0300

    Scripted auto-commit on change (2021-12-18 01:45:19) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -13,13 +13,14 @@ import java.util.concurrent.CompletionStage;
 public class TaskTestingApp {
     private static final int PORT = 8088;
     private static final String HOST = "localhost";
-    
+
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("webtest");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         //final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
         //final CompletionStage<ServerBinding> binding = http.bindAndHandle()
-        System.out.println("Server online at https://%s%d/\nPress ENTER to stop", );
+        System.out.printf("Server online at https://%s%d/\nPress ENTER to stop\n", HOST, PORT);
+        Sy
     }
 }
