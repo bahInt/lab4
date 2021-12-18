d08e95fe747895a5800597e065ce2aed66e3e3a8
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 05:36:02 +0300

    Scripted auto-commit on change (2021-12-18 05:36:02) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -4,15 +4,16 @@ import akka.actor.AbstractActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
+import lab4.messages.ExecMessage;
 import lab4.messages.PutMessage;
 
 public class ExecutorActor extends AbstractActor {
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), self());
 
     private void sendToStorage(ExecMessage r) {
-        sender().tell(new PutMessage(r.getPackID));
+        sender().tell(new PutMessage(r.getPackID()));
     }
-}
+
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
@@ -20,3 +21,4 @@ public class ExecutorActor extends AbstractActor {
                 .matchAny(o -> log.info("received unknown message"))
                 .build();
     }
+}
