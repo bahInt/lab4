1a9d45d6c50ef10f40f2aa4d5f3a12c023c3758a
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 03:20:13 +0300

    Scripted auto-commit on change (2021-12-18 03:20:13) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -6,11 +6,12 @@ import lab4.assists.GetMessage;
 import lab4.assists.PutMessage;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StorageActor extends AbstractActor {
-    private Map<String, String> storage = new HashMap<>();
+    private Map<String, ArrayList<String>> storage = new HashMap<>();
 
     @Override
     public Receive createReceive() {
