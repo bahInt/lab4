e317e18f48968d6877e80c91c242b929f2dd8ac2
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 07:11:37 +0300

    Scripted auto-commit on change (2021-12-18 07:11:37) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -29,8 +29,8 @@ public class TaskTestingApp {
         LoggingAdapter l = Logging.getLogger(system, LOG_SOURCE);
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        //final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-        //final CompletionStage<ServerBinding> binding = http.bindAndHandle()
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle()
         System.out.printf("Server online at https://%s%d/\nPress ENTER to stop\n", HOST, PORT);
         System.in.read();
         //binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
