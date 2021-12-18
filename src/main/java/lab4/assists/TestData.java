ffd4f9d65114e5376b703786e2d8b1c5175268a2
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 05:03:52 +0300

    Scripted auto-commit on change (2021-12-18 05:03:52) by gitwatch.sh

--- src/main/java/lab4/assists/TestData.java
+++ src/main/java/lab4/assists/TestData.java
@@ -1,6 +1,14 @@
 package lab4.assists;
 
+import java.util.ArrayList;
+
 public class TestData {
     private  final String testName;
-    
+    private final String expectedResult;
+    private final ArrayList<Object> params;
+
+    public TestData(String testName, String expectedResult, ArrayList<Object> params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+    }
 }
