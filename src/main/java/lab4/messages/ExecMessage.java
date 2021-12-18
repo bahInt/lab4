9547ea020aa29a6145faa0870951fdae54d55cd8
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 04:52:09 +0300

    Scripted auto-commit on change (2021-12-18 04:52:09) by gitwatch.sh

--- src/main/java/lab4/messages/ExecMessage.java
+++ src/main/java/lab4/messages/ExecMessage.java
@@ -1,7 +1,10 @@
 package lab4.messages;
 
+import java.util.ArrayList;
+
 public class ExecMessage {
-    private final String packID, funcName, testName;
+    private final String packID, funcName, jsScript, testName, expRes;
+    private final ArrayList<Object> params;
 
     public ExecMessage(String packID, String funcName) {}
 
