3b6521f266fe3d84d6d850e7c211114dd9bf38ed
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 07:01:32 +0300

    Scripted auto-commit on change (2021-12-18 07:01:32) by gitwatch.sh

--- src/main/java/lab4/assists/HttpParse.java
+++ src/main/java/lab4/assists/HttpParse.java
@@ -27,7 +27,7 @@ public class HttpParse {
                     Future<Object> future = Patterns.ask(router, new GetMessage(pID), TIMEOUT);
                     return completeOKWithFuture(future, Jackson.marshaller());
                 })),
-                post()
+                post(() -> entity(Jackson.unmarshaller(PackageData.class),)
         );
     }
 }
