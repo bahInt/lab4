d0fe146d6d6a00ac326ffcfe95ea5180dfe6a47a
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 03:18:29 +0300

    Scripted auto-commit on change (2021-12-18 03:18:29) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -1,8 +1,11 @@
 package lab4.actors;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class ExecutorActor extends AbstractActor {
 
-    
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+    }
 }
