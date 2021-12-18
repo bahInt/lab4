2d2cfd381eaaca6bb7795668fff22b3311780b7f
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 07:09:55 +0300

    Scripted auto-commit on change (2021-12-18 07:09:55) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -1,7 +1,11 @@
 package lab4;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.pattern.Patterns;
@@ -9,6 +13,8 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import lab4.actors.RouterActor;
+
 import java.util.concurrent.CompletionStage;
 
 public class TaskTestingApp {
@@ -18,6 +24,8 @@ public class TaskTestingApp {
 
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create(SYS_NAME);
+        ActorRef routerActor = system.actorOf(Props.create(RouterActor.class), "router");
+        LoggingAdapter l = Logging.getLogger(system, LOG_SOURCE);
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         //final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
