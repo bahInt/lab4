f8edd6880eef22425eb14e01e98f03901ed70a43
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 06:07:19 +0300

    Scripted auto-commit on change (2021-12-18 06:07:19) by gitwatch.sh

--- src/main/java/lab4/actors/RouterActor.java
+++ src/main/java/lab4/actors/RouterActor.java
@@ -1,10 +1,14 @@
 package lab4.actors;
 
 import akka.actor.AbstractActor;
+import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 
 public class RouterActor extends AbstractActor {
-    public RouterActor() {}
+    public RouterActor() {
+        storage = getContext().actorOf(Props.create(StorageActor.class), STORAGE_NAME);
+        getContext().watch(storage);
+    }
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
