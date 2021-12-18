4bd3ac3a11c9dda0dd7d8b9e0a4e6b82b658c11a
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 06:59:31 +0300

    Scripted auto-commit on change (2021-12-18 06:59:31) by gitwatch.sh

--- src/main/java/lab4/assists/HttpParse.java
+++ src/main/java/lab4/assists/HttpParse.java
@@ -3,14 +3,18 @@ package lab4.assists;
 import akka.actor.ActorRef;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
+import akka.util.Timeout;
 import lab4.messages.GetMessage;
+import scala.concurrent.Future;
 
-import java.util.concurrent.Future;
+import java.time.Duration;
+
 
 import static akka.http.javadsl.server.Directives.*;
 
 public class HttpParse {
     private final ActorRef router;
+    private final static Timeout TIMEOUT = Timeout.create(Duration.ofSeconds(5));
 
     public HttpParse(ActorRef router) {
         this.router = router;
