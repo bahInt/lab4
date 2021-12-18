f72819cdbce486d2aca3f823e9d3ed1144d8d341
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 07:14:09 +0300

    Scripted auto-commit on change (2021-12-18 07:14:09) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -6,6 +6,7 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.pattern.Patterns;
@@ -32,7 +33,11 @@ public class TaskTestingApp {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         HttpParse instance = new HttpParse(routerActor);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle()
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
         System.out.printf("Server online at https://%s%d/\nPress ENTER to stop\n", HOST, PORT);
         System.in.read();
         //binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
