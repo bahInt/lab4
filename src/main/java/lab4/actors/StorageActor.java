f1a49da374de07c8c4a2b5f8a8fb1c69e69ec87b
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 03:51:36 +0300

    Scripted auto-commit on change (2021-12-18 03:51:36) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -7,17 +7,18 @@ import lab4.assists.PutMessage;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
+
+import java.util.Hashtable;
 import java.util.Map;
 
 public class StorageActor extends AbstractActor {
-    private Map<String, ArrayList<String>> storage = new HashMap<>();
+    private Map<String, ArrayList<String>> storage = new Hashtable<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(PutMessage.class, r ->
-                    storage.put(r.getPackID(), r.getResult()))
+                .match(PutMessage.class, r -> {
+                    ArrayList<String> results = storage.get())
                 .match(GetMessage.class, r ->
                         sender().tell(new PutMessage(r.getPackID(), storage.get(r.getPackID())), self()))
                 .build();
