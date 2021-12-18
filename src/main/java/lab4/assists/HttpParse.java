21e80436e105bde62346ab707efdaccc55ddeb88
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 06:46:00 +0300

    Scripted auto-commit on change (2021-12-18 06:46:00) by gitwatch.sh

--- src/main/java/lab4/assists/HttpParse.java
+++ src/main/java/lab4/assists/HttpParse.java
@@ -1,5 +1,11 @@
 package lab4.assists;
 
+import akka.actor.ActorRef;
+
 public class HttpParse {
-    
+    private final ActorRef router;
+
+    public HttpParse(ActorRef router) {
+        this.router = router;
+    }
 }
