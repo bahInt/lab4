eb2a275a1e56d87cd20b97ce41ed9000ccaab568
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 05:26:43 +0300

    Scripted auto-commit on change (2021-12-18 05:26:43) by gitwatch.sh

--- src/main/java/lab4/assists/PackageData.java
+++ src/main/java/lab4/assists/PackageData.java
@@ -2,7 +2,15 @@ package lab4.assists;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 public class PackageData {
     @JsonProperty("packageId")
     private final String packageId;
+    @JsonProperty("jsScript")
+    private final String jsScript;
+    @JsonProperty("functionName")
+    private final String functionName;
+    @JsonProperty("tests")
+    private final ArrayList<TestData> tests;
 }
