7ba1cb11a48ddfa820ed708a82f00c1f562e72f7
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 03:18:56 +0300

    Scripted auto-commit on change (2021-12-18 03:18:56) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -2,10 +2,18 @@ package lab4.actors;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import lab4.assists.GetMessage;
+import lab4.assists.PutMessage;
 
 public class ExecutorActor extends AbstractActor {
 
+    @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
+                .match(PutMessage.class, r -> {
+                    storage.put(r.getPackID(), r.getResult());
+                })
+                .match(GetMessage.class, r ->
+                        sender().tell(new PutMessage(r.getPackID(), storage.get(r.getPackID())), self()))
+                .build();
     }
-}
--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -12,14 +12,5 @@ import java.util.Map;
 public class StorageActor extends AbstractActor {
     private Map<String, String> storage = new HashMap<>();
 
-    @Override
-    public Receive createReceive() {
-        return ReceiveBuilder.create()
-                .match(PutMessage.class, r -> {
-                    storage.put(r.getPackID(), r.getResult());
-                })
-                .match(GetMessage.class, r ->
-                        sender().tell(new PutMessage(r.getPackID(), storage.get(r.getPackID())), self()))
-                .build();
-    }
+
 }
