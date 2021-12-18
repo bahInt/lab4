b885f625cc1c4be691c7af0e6e99fad4d4f27041
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 03:19:22 +0300

    Scripted auto-commit on change (2021-12-18 03:19:22) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -10,10 +10,5 @@ public class ExecutorActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(PutMessage.class, r -> {
-                    storage.put(r.getPackID(), r.getResult());
-                })
-                .match(GetMessage.class, r ->
-                        sender().tell(new PutMessage(r.getPackID(), storage.get(r.getPackID())), self()))
-                .build();
+                
     }
