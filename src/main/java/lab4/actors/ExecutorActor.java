85458f8d6b18b85f33b5b12752490db27f0e843e
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-18 05:39:23 +0300

    Scripted auto-commit on change (2021-12-18 05:39:23) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -7,12 +7,19 @@ import akka.japi.pf.ReceiveBuilder;
 import lab4.messages.ExecMessage;
 import lab4.messages.PutMessage;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class ExecutorActor extends AbstractActor {
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), self());
 
     private String execute(ExecMessage r) {
         String result;
-        
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
     }
 
     private void sendToStorage(ExecMessage r) {
